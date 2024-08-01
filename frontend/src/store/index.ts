6e4bcb69479a0f0a022d3fda9f5ca92804f4bc96
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-08-01 20:20:46 +0300

    frontend: redux - changed in good shape.

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import invoiceReducer from '../features/invoices/invoiceSlice';
 import authReducer from './slices/authSlice';
-import invoiceReducer from './slices/invoiceSlice';
 
 const store = configureStore({
   reducer: {
