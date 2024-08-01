c6aec3fd0c1a706e8791fd039a7b06a8aabafb54
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-08-01 20:19:11 +0300

    frontend: minor changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import './index.css';
-import store from "./store";
+import store from './store';
 
 const queryClient = new QueryClient();
 
