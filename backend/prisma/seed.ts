07188c18e66fcbdfcdf48fde5586c00022b40bb9
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-08-01 20:17:40 +0300

    backend: prisma - Added more data for the seed.

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,30 +4,43 @@ import * as bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
-  const password = bcrypt.hashSync('password', 10);
-  const user = await prisma.user.create({
+  const password1 = bcrypt.hashSync('password', 10);
+  const password2 = bcrypt.hashSync('password', 10);
+  const password3 = bcrypt.hashSync('password', 10);
+  
+  const user1 = await prisma.user.create({
     data: {
       email: 'user@example.com',
-      password,
+      password: password1,
     },
   });
-
+  
+  const user2 = await prisma.user.create({
+    data: {
+      email: 'user2@example.com',
+      password: password2,
+    },
+  });
+  
+  const user3 = await prisma.user.create({
+    data: {
+      email: 'user3@example.com',
+      password: password3,
+    },
+  });
+  
   await prisma.invoice.createMany({
     data: [
-      {
-        vendor_name: 'Vendor 1',
-        amount: 100,
-        due_date: new Date(),
-        user_id: user.id,
-        paid: false,
-      },
-      {
-        vendor_name: 'Vendor 2',
-        amount: 200,
-        due_date: new Date(),
-        user_id: user.id,
-        paid: true,
-      },
+      { vendor_name: 'Vendor 1', amount: 100, description: "Rental" , due_date: new Date(), user_id: user1.id, paid: false },
+      { vendor_name: 'Vendor 2', amount: 200, description: "Rental", due_date: new Date(), user_id: user1.id, paid: true },
+      { vendor_name: 'Vendor 3', amount: 300, description: "Rental", due_date: new Date(), user_id: user1.id, paid: false },
+      { vendor_name: 'Vendor 4', amount: 400, description: "Rental", due_date: new Date(), user_id: user2.id, paid: true },
+      { vendor_name: 'Vendor 5', amount: 500, description: "Rental", due_date: new Date(), user_id: user2.id, paid: false },
+      { vendor_name: 'Vendor 6', amount: 600, description: "Rental", due_date: new Date(), user_id: user2.id, paid: true },
+      { vendor_name: 'Vendor 7', amount: 700, description: "Rental", due_date: new Date(), user_id: user3.id, paid: false },
+      { vendor_name: 'Vendor 8', amount: 800, description: "Rental", due_date: new Date(), user_id: user3.id, paid: true },
+      { vendor_name: 'Vendor 9', amount: 900, description: "Rental", due_date: new Date(), user_id: user3.id, paid: false },
+      { vendor_name: 'Vendor 10', amount: 1000, description: "Rental", due_date: new Date(), user_id: user3.id, paid: true },
     ],
   });
 }
